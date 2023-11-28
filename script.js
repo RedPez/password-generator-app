685dbd74cb3191e252cd44e668a19c72f2d5eea4
unknown <pjrbusiness@outlook.com>
2023-11-28 17:11:51 +0000

    Started drafting steps to complete. Added event listener and prompts for user input

--- script.js
+++ script.js
@@ -88,45 +88,58 @@ var upperCasedCharacters = [
   "Z",
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+/* Steps to develop generator:
+
+1. Add event listener. When 'Generate Password' button is clicked this begins the initial prompt. 
+
+2. Series of prompts to collect user input/data. 
+
+3. Validate the data - return error, subsequent promt if incorrect data gathered. Must choose at least one character type. 
+
+4.  Answer to each prompt to be stored in a variable. [would invidiual variables be needed or would an array work?] 
+
+5. Generate password. Either via an alert or displayed on the page. */
+
+//Start Code
+
+//1. Add event listener &&
+// 2. Series of prompts collecting user for password options.
+
+const generate = document.querySelector("#generate");
+
+generate.addEventListener("click", function () {
   let passwordLengthPrompt = prompt(
     "How long would you like your password to be? Please enter a number between 8 and 128"
   );
+
+  //i.convert passwordlength to a number?....
+  //ii. If statement
+
+  //If (password is not a number or < 8 or >128 then ) {
+  //promp("Please enter a valid number between 8 and 128!");
+  //return; }
 
   let lowerCasePrompt = prompt(
     "Would you like your password to contain Lowercase letters? Answer yes or no"
   );
 
+  //i.convert passwordlength to a true/false statement?....
+
   let uppercaseCasePrompt = prompt(
     "Would you like your password to contain Uppercase letters? Answer yes or no"
   );
+
+  //i.convert passwordlength to a true/false statement?....
 
   let numbersPrompt = prompt(
     "Would you like your password to contain Numbers? Answer yes or no"
   );
 
+  //i.convert passwordlength to a true/false statement?....
+
   let specialCharactersPrompt = prompt(
     "Would you like your password to contain Special characters? Like $!*&. Answer yes or no"
   );
-}
 
-/*// Function for getting a random element from an array
-function getRandom(arr) {}
-
-// Function to generate password with user input
-function generatePassword() {}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);*/
+  //i.convert passwordlength to a true/false statement?....
+});
