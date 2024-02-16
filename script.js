f7cb31d40fffd7bda4846ad17e3fa3c21fe60bbe
unknown <pjrbusiness@outlook.com>
2024-02-16 18:51:20 +0000

    Updated project with current JS knowledge

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Array of special characters to be included in password
+// Special character array
 var specialCharacters = [
   "@",
   "%",
@@ -25,10 +25,10 @@ var specialCharacters = [
   ".",
 ];
 
-// Array of numeric characters to be included in password
+// Numbers array
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// Array of lowercase characters to be included in password
+// Lowercase letters array
 var lowerCasedCharacters = [
   "a",
   "b",
@@ -58,7 +58,7 @@ var lowerCasedCharacters = [
   "z",
 ];
 
-// Array of uppercase characters to be included in password
+// Uppercase letters array
 var upperCasedCharacters = [
   "A",
   "B",
@@ -88,130 +88,87 @@ var upperCasedCharacters = [
   "Z",
 ];
 
-/* Steps to develop generator:
+// Random element array function
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
-1. Add event listener. When 'Generate Password' button is clicked this begins the initial prompt. 
+// Generate passwords array
+function generatePassword(length, useLower, useUpper, useNumbers, useSpecial) {
+  let password = "";
+  let possibleCharacters = [];
 
-2. Series of prompts to collect user input/data. 
+  if (useLower) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+  }
 
-3. Validate the data - return error, subsequent promt if incorrect data gathered. Must choose at least one character type. 
+  if (useUpper) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+  }
 
-4.  Answer to each prompt to be stored in a variable. [would invidiual variables be needed or would an array work?] 
+  if (useNumbers) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+  }
 
-5. Generate password. Either via an alert or displayed on the page. */
+  if (useSpecial) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+  }
 
-//Start Code
+  if (possibleCharacters.length === 0) {
+    alert("You must select at least one character type!");
+    return "";
+  }
 
-//1. Add event listener &&
-// 2. Series of prompts collecting user for password options.
+  for (let i = 0; i < length; i++) {
+    password += getRandomElement(possibleCharacters);
+  }
 
-const generate = document.querySelector("#generate");
+  return password;
+}
 
-generate.addEventListener("click", function () {
-  //password length prompt
+const generateButton = document.querySelector("#generate");
+const passwordDisplay = document.querySelector("#password");
+
+generateButton.addEventListener("click", function () {
   let passwordLengthPrompt = prompt(
     "How long would you like your password to be? Please enter a number between 8 and 128"
   );
-  // If statement for password length
-  if (
-    /*passwordLengthPrompt is not a number || */ passwordLengthPrompt < 8 ||
-    passwordLengthPrompt > 128
-  ) {
+
+  let passwordLength = parseInt(passwordLengthPrompt);
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a valid number between 8 and 128!");
     return;
-  } else {
-    //Convert passwordlength to a number?....
-    let passwordlength = parseInt(passwordLengthPrompt);
-    console.log(passwordlength);
   }
 
-  //lowercase prompt
   let lowerCasePrompt = prompt(
-    "Would you like your password to contain Lowercase letters? Answer yes or no"
+    "Would you like your password to contain lowercase letters? Answer yes or no"
   );
+  let useLower = lowerCasePrompt.toLowerCase() === "yes";
 
-  //Validate string - add in more alternatives for yes and no spellings etc. Can you just use "Y" instead of "yes"?
-  //let useLowerCase = lowerCasePrompt.toLowerCase() === "y";
-
-  // Or should I do if statements instead? for lowercase prompt
-
-  if (
-    lowerCasePrompt !== "yes" &&
-    lowerCasePrompt !== "Yes" &&
-    lowerCasePrompt !== "y" &&
-    lowerCasePrompt !== "Y" &&
-    lowerCasePrompt !== "no" &&
-    lowerCasePrompt !== "No" &&
-    lowerCasePrompt !== "n" &&
-    lowerCasePrompt !== "N"
-  ) {
-    prompt("Please enter yes or no");
-  } else {
-    lowerCasePrompt = prompt("Please enter yes or no");
-  } //Hmmm still not working. Should I do a while loop?
-
-  console.log(lowerCasePrompt);
-
-  //prompt for upper case characters
   let upperCasePrompt = prompt(
-    "Would you like your password to contain Uppercase letters? Answer yes or no"
+    "Would you like your password to contain uppercase letters? Answer yes or no"
   );
+  let useUpper = upperCasePrompt.toLowerCase() === "yes";
 
-  if (
-    upperCasePrompt !== "yes" &&
-    upperCasePrompt !== "Yes" &&
-    upperCasePrompt !== "y" &&
-    upperCasePrompt !== "Y" &&
-    upperCasePrompt !== "no" &&
-    upperCasePrompt !== "No" &&
-    upperCasePrompt !== "n" &&
-    upperCasePrompt !== "N"
-  ) {
-    prompt("Please enter yes or no");
-  } else {
-    //log the prompt to upperCasePrompt
-  }
-  console.log(upperCasePrompt);
-
-  //number character prompt
   let numbersPrompt = prompt(
-    "Would you like your password to contain Numbers? Answer yes or no"
+    "Would you like your password to contain numbers? Answer yes or no"
   );
+  let useNumbers = numbersPrompt.toLowerCase() === "yes";
 
-  if (
-    numbersPrompt !== "yes" &&
-    numbersPrompt !== "Yes" &&
-    numbersPrompt !== "y" &&
-    numbersPrompt !== "Y" &&
-    numbersPrompt !== "no" &&
-    numbersPrompt !== "No" &&
-    numbersPrompt !== "n" &&
-    numbersPrompt !== "N"
-  ) {
-    prompt("Please enter yes or no");
-  } else {
-    //log the prompt to numbersPrompt
-  }
-  console.log(numbersPrompt);
-
-  //Special characters prompt
   let specialCharactersPrompt = prompt(
-    "Would you like your password to contain Special characters? Like $!*&. Answer yes or no"
+    "Would you like your password to contain special characters? Like $!*&. Answer yes or no"
+  );
+  let useSpecial = specialCharactersPrompt.toLowerCase() === "yes";
+
+  let password = generatePassword(
+    passwordLength,
+    useLower,
+    useUpper,
+    useNumbers,
+    useSpecial
   );
 
-  if (
-    specialCharactersPrompt !== "yes" &&
-    specialCharactersPrompt !== "Yes" &&
-    specialCharactersPrompt !== "y" &&
-    specialCharactersPrompt !== "Y" &&
-    specialCharactersPrompt !== "no" &&
-    specialCharactersPrompt !== "No" &&
-    specialCharactersPrompt !== "n" &&
-    specialCharactersPrompt !== "N"
-  ) {
-    prompt("Please enter yes or no");
-  } else {
-    //log the prompt to lowerCasePrompt
+  if (password !== "") {
+    passwordDisplay.textContent = password;
   }
-  console.log(specialCharactersPrompt);
 });
